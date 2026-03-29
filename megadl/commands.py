7b06a6438a2ece75d08cbd05843099642633fec6
allentertainmentsstudio <allentertainmentsstudio@gmail.com>
2026-03-29 17:50:33 +0530

    Update commands.py

--- megadl/commands.py
+++ megadl/commands.py
@@ -138,7 +138,7 @@ async def refreshmeh_cb(bot, message):
             user = await bot.get_chat_member(int(Config.UPDATES_CHANNEL), message.from_user.id)
             if user.status == "kicked":
                 await message.message.edit(
-                    text="Sorry Sir, You are Banned. Contact My [Support Group](https://t.me/AsmSupport).",
+                    text="Sorry Sir, You are Banned. Contact My [Support Group](https://t.me/log_channel_a).",
                     parse_mode="markdown",
                     disable_web_page_preview=True
                 )
@@ -161,7 +161,7 @@ async def refreshmeh_cb(bot, message):
             return
         except Exception:
             await message.message.edit(
-                text="Something Went Wrong. Contact My [Support Group](https://t.me/AsmSupport).",
+                text="Something Went Wrong. Contact My [Support Group](https://t.me/log_channel_a).",
                 parse_mode="markdown",
                 disable_web_page_preview=True
             )
